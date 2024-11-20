5ee3e3e82e221ba06a078c9cffcf7a53a01e9d5e
Serhii Bolhovskyi <serhiibolhovskyi@MacBook-Air-Serhii.local>
2024-11-20 20:57:01 +0100

    Solution for b and c

--- While/Program.cs
+++ While/Program.cs
@@ -1,6 +1,7 @@
 ﻿int n;
+int factorial = 1;
 bool isValid;
-double a = 0, b, c;
+double a = 0, b = 1, c = 0;
 
 do
 {
@@ -10,7 +11,21 @@ do
         Console.WriteLine("Please enter a valid positive integer!");
 } while(!isValid || n < 1);
 
+// a
 for (int i = 1; i <= n; i++)
     a += Math.Pow(-1, i) / (2 * i + 1);
 
-Console.WriteLine($"a = {a:F4}");
+Console.WriteLine($"a = {a:F3}");
+
+// b
+for(int i = 1; i <= n; i++)
+    b += 1.0/Math.Pow(i, 2);
+Console.WriteLine($"b = {b:F3}");
+
+//c
+for (int i = 1; i <= n; i++)
+{
+    factorial *= i;
+    c = factorial;
+}
+Console.WriteLine($"c = {factorial:F1}");
