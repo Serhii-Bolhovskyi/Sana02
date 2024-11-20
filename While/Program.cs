1ef6fca8613e6210c5c679d894d1242206989941
Serhii Bolhovskyi <serhiibolhovskyi@MacBook-Air-Serhii.local>
2024-11-20 20:11:46 +0100

    Add 4th project and solution for a variable

---
+++ While/Program.cs
@@ -0,0 +1,16 @@
+﻿int n;
+bool isValid;
+double a = 0, b, c;
+
+do
+{
+    Console.Write("Enter n: ");
+    isValid = int.TryParse(Console.ReadLine(), out n);
+    if(!isValid || n < 1)
+        Console.WriteLine("Please enter a valid positive integer!");
+} while(!isValid || n < 1);
+
+for (int i = 1; i <= n; i++)
+    a += Math.Pow(-1, i) / (2 * i + 1);
+
+Console.WriteLine($"a = {a:F4}");
